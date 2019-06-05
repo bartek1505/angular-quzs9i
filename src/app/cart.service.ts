e2866a8fe4e331ff04aa129ca4b6ea2131a3d352
bartek1505 <boruc.b@gmail.com>
2019-06-05 19:40:32 +0200

    4 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,4 +7,18 @@ export class CartService {
 
   constructor() { }
 
+items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
