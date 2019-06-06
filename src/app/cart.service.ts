3f27fca0e458aadee87cdc17b681c36e244e45a0
bartek1505 <boruc.b@gmail.com>
2019-06-06 10:02:31 +0200

    Gotowa apka wg tutorialu

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  items = [];
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
 
-items = [];
 
   addToCart(product) {
     this.items.push(product);
@@ -20,5 +23,9 @@ items = [];
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }
